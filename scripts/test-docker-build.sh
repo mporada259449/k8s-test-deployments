29058605e5001559a3f124a90c0915c4c8a19064
mporada259449 <259449@student.pwr.edu.pl>
2025-02-19 23:34:36 +0100

    Adding testing script

--- scripts/test-docker-build.sh
+++ scripts/test-docker-build.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-ls -la
+container_name=yolo
+docker build -t $container_name model/
+docker run --rm -d $container_name
+exit_code=$?
+if [ $exit_code ]; then
+    echo "Container run with successfully"
+else
+    echo "Container startup faild with code $exit_code"
+fi
