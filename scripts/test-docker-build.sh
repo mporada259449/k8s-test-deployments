62a7a85a0250b03b5e48087a6521e125b23dfe7e
mporada259449 <259449@student.pwr.edu.pl>
2025-03-16 18:59:59 +0100

    Further test

--- scripts/test-docker-build.sh
+++ scripts/test-docker-build.sh
@@ -7,15 +7,13 @@ exit_code=$?
 if [ $exit_code -eq 0 ]; then
     echo "Container run successfully"
     curl -L -O https://ultralytics.com/images/bus.jpg
-    inference_code=$(curl -X POST -F "file=@bus.jpg" -o /dev/null -s -w "%{http_code}\n" 127.0.0.1:5000/predict)
-    if [ $inference_code=='200' ]; then
-        echo "Inference of image passed"
-    else
-        echo "Inference failed with status code ${inference_code}"
-        curl -X POST -F "file=@bus.jpg" -o /dev/null -s -w "%{http_code}\n" 127.0.0.1:5000/predict
+    inference_result=$(curl -X POST -F "file=@bus.jpg" 127.0.0.1:5000/predict)
+    if grep -q 'Error' <<< "${inference_result}"; then
+        echo "Inference failed with result: ${inference_result}"
         exit 1
+    else
+        echo "Inference of image passed"
     fi
-
 else
     echo "Container startup failed with code $exit_code"
     exit 1
