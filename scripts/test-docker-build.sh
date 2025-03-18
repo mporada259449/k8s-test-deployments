7c426defef99f979081d1af85e3f5c8499f21a02
mporada259449 <81997217+mporada259449@users.noreply.github.com>
2025-03-18 19:05:31 +0100

    Aligning with master - plus deleting image fetching(#11)
    * Tests jobs improvment (#2)
    
    Fixes on test flow and version bumping check:
    * version.json has to be change if model/ will be changed
    * some other small changes
    
    * Improvment for building, pushing images and tagging commits (#4)
    
    What is new:
    
    New images are builded and pushed with predefined step
    Metadata step were added - it is not currently used
    Auto tagging commit for new images is introduced
    Minor fixes - mostly when pipelines starts
    
    * Server run with onnx file  (#6)
    
    Server uses ONNX file instead of pt file to load model. With new approche additional preprocessing was needed defined in main.py file
    
    Docker Image size reduction form 10GB to 1.5GB
    
    Dockerfile has 2 stages now
    1. Export of model to ONNX file and export of used classes
    2. Creating and runnig FastAPI server
    
    * Aliging version with master branch - ONNX file fix
    
    * Enhanced docker image testing - test inference (#8)
    
    Now during testing server will not be only run but also request for inference will be send
    
    * Build docker - change when image is built (#10)
    
    * Modification of test script - inference of image commited in repo

--- scripts/test-docker-build.sh
+++ scripts/test-docker-build.sh
@@ -2,11 +2,19 @@
 set -eux
 container_name=yolo
 docker build -t $container_name --build-arg YOLO_FILE=${YOLO_FILE} model/
-docker run --rm -d $container_name
+docker run --rm -p 5000:5000 -d $container_name
 exit_code=$?
 if [ $exit_code -eq 0 ]; then
     echo "Container run successfully"
+    sleep 10
+    inference_result=$(curl -X POST -F "file=@scripts/test-data/bus.jpg" 127.0.0.1:5000/predict)
+    if grep -q 'Error' <<< "${inference_result}"; then
+        echo "Inference failed with result: ${inference_result}"
+        exit 1
+    else
+        echo "Inference of image passed"
+    fi
 else
-    echo "Container startup faild with code $exit_code"
+    echo "Container startup failed with code $exit_code"
     exit 1
 fi
