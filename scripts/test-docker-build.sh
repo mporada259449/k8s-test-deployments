43816b68f64f9c921f928b9d651aecc3e2260c38
mporada259449 <81997217+mporada259449@users.noreply.github.com>
2025-03-18 19:17:05 +0100

    Stop fetch image with curl - test data usage (#12)

--- scripts/test-docker-build.sh
+++ scripts/test-docker-build.sh
@@ -6,8 +6,8 @@ docker run --rm -p 5000:5000 -d $container_name
 exit_code=$?
 if [ $exit_code -eq 0 ]; then
     echo "Container run successfully"
-    curl -L -O https://ultralytics.com/images/bus.jpg
-    inference_result=$(curl -X POST -F "file=@bus.jpg" 127.0.0.1:5000/predict)
+    sleep 10
+    inference_result=$(curl -X POST -F "file=@scripts/test-data/bus.jpg" 127.0.0.1:5000/predict)
     if grep -q 'Error' <<< "${inference_result}"; then
         echo "Inference failed with result: ${inference_result}"
         exit 1
